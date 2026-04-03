c1b4b66cce5dbd653088fe21d23832010b6255c5
mirleyik9090-ctrl <mirleyik9090@gmail.com>
2026-04-03 16:15:17 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,4 @@
 <?php
 return [
-    // Isso faz o bot ler a chave que você colocou no Railway
     'OPENROUTER_KEY' => getenv('OPENROUTER_KEY') ?: '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',
 ];
